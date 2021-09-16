01adf4c61eac181a3667cea051f2aa88611fbb3e
BriagasD98 <briagasdavid@yahoo.com>
2021-09-16 00:34:51 -0500

    Photolist and Gallery components added, tests created and passed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-import './App.css';
+// import './App.css';
 
 function App() {
   const [categories] = useState([
@@ -16,19 +16,18 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        ></Nav>
-        <main>
-          <div>
-            <Gallery></Gallery>
-            <About></About>
-          </div>
-        </main>
+      ></Nav>
+      <main>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+      </main>
     </div>
   );
 }
